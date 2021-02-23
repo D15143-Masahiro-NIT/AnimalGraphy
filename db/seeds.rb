f6b76cf0b1c9c534e5380322dec50dfd64d1a47c
masahiro <d15143.masahiro.nit@gmail.com>
2021-02-24 01:35:59 +0900

    seeds.rbにユーザー作成の実装を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development?
+  25.times do
+    name = Faker::Name.name
+    email = Faker::Internet.email
+    User.create!(name: name, email: email, password: 'password')
+  end
+end
